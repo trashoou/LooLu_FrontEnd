8119fc152d690bd769198203e19e3ffeaa5d1d5e
Dovlet <allakov.dovlet@gmail.com>
2024-06-07 19:10:30 +0300

    добавлены комментарии

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,26 @@
+//Импорты
+//NavLink: Импортируется из react-router-dom для создания навигационных ссылок, 
+//которые позволяют переключаться между различными страницами/маршрутами приложения.
+//styles: Импортируются стили из CSS-модуля Header.module.css, что позволяет использовать CSS-классы, 
+//определенные в этом файле, внутри компонента.
 import { NavLink } from 'react-router-dom'
 
 import styles from './Header.module.css';
 
-const Header = () => {
+const Header = () => { //Определяем функциональный компонент Header.
     return (
+        // Основной контейнер для навигационного меню, стилизованный с помощью CSS-модуля.
         <div className={styles.container}>
+             {/* Список ul для навигационных ссылок. */}
                 <ul className={styles.list__container}>
+                    {/* Элемент списка с навигационной ссылкой на домашнюю страницу. */}
                     <li><NavLink to="/" exact >Home</NavLink></li>
+                    {/* Элемент списка с навигационной ссылкой на страницу продуктов. */}
                     <li><NavLink to="/products" exact >Products</NavLink></li>
                 </ul>
         </div>
     )
 }
 
+// Экспортируем компонент для использования в других частях приложения.
 export default Header;
--- src/components/ProductsPage/ProductsList/ProductsList.jsx
+++ src/components/ProductsPage/ProductsList/ProductsList.jsx
@@ -1,13 +1,20 @@
 import PropTypes from 'prop-types'; // ES6
 import styles from './ProductsList.module.css';
 
+// Определение функционального компонента ProductsList.
+// Используем деструктуризацию для получения пропса `products`.
 const ProductsList = ({ products }) => {
     return (
+         // Рендерим список ul с классом из стилей.
         <ul className={styles.list__container}>
+            {/* Перебираем массив products и рендерим каждый продукт в виде элемента списка li. */}
             {products.map(({ title, picture }) => 
+                // Каждому элементу списка задаем уникальный ключ key, используя title.
                 <li className={styles.list__item} key={title}>
                     <a href="#">
+                        {/* Рендерим изображение продукта с источником picture и альтернативным текстом title. */}
                         <img className={styles.product__photo} src={picture} alt={title}/>
+                        {/* Рендерим название продукта. */}
                         <p>{title}</p>
                     </a>
                 </li>
@@ -16,8 +23,11 @@ const ProductsList = ({ products }) => {
     )
 }
 
+// Определение PropTypes для компонента.
+// Ожидается, что `products` будет массивом объектов.
 ProductsList.propTypes = {
     products: PropTypes.array
 }
 
+// Экспортируем компонент для использования в других местах.
 export default ProductsList;
