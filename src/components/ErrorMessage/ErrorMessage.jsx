e8a8f6bb06c4ccd6520d9c08ec5ddae5d415e0bd
Dovlet <allakov.dovlet@gmail.com>
2024-06-07 19:20:46 +0300

    add comment

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -2,17 +2,22 @@ import styles from './ErrorMessage.module.css';
 import icon from '../image/Loolu_big.webp';
 //import LoadSpinner from '../LoadSpinner/LoadSpinner';
 
+// Определение функционального компонента ErrorMessage.
 const ErrorMessage = () => {
     return (
+        // React Fragment, используемый для группировки дочерних элементов.
         <>
+        {/* Текст сообщения об ошибке с применением стилей из CSS-модуля. */}
             <p className={styles.text}>
                 We cannot display data. <br />
                 Come back when we fix everything
                 {/*<LoadSpinner />*/}
+                {/* Изображение с иконкой, стилизованное с помощью CSS-модуля. */}
                 <img className={styles.loolu_icon} src={icon} alt="" />
             </p>
         </>
     )
 }
 
+// Экспортируем компонент для использования в других частях приложения.
 export default ErrorMessage;
